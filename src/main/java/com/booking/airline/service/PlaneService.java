7103a72c92b0048d2167bfa75925174961a8d18a
patilpratikha <pratikshanila76@gmail.com>
2024-07-11 15:47:05 +0530

    small changes in plane

--- src/main/java/com/booking/airline/service/PlaneService.java
+++ src/main/java/com/booking/airline/service/PlaneService.java
@@ -1,16 +1,12 @@
 package com.booking.airline.service;
 
-import com.booking.airline.model.Login;
 import com.booking.airline.model.Plane;
-import com.booking.airline.model.User;
 import com.booking.airline.repository.PlaneRepository;
-import com.booking.airline.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.lang.module.ResolutionException;
 import java.util.List;
 import java.util.Optional;
 @Service
@@ -19,14 +15,14 @@ public class PlaneService {
     private PlaneRepository planeRepository;
 
     public ResponseEntity<String> addPlaneDetails(Plane plane) {
-        Optional<Plane> existingPlane = planeRepository.findById(plane.getId());
-        if (existingPlane.isEmpty()) {        //Add Id
+
+        if (plane!=null) {        //Add Id
             planeRepository.save(plane);
-            return new ResponseEntity<>("plane id added successfully", HttpStatus.OK);
+            return new ResponseEntity<>("plane added successfully", HttpStatus.OK);
         }
         //plane detail are existing
         else {
-            return new ResponseEntity<String>("plane id is already existing", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<String>("details not valid", HttpStatus.BAD_REQUEST);
         }
 
     }
@@ -53,13 +49,7 @@ public class PlaneService {
         if (planeInfo.isPresent()) {
             Plane existingPlane = planeInfo.get();    //persisted user info object
             existingPlane.setName(plane.getName());
-            existingPlane.setSpeed(plane.getSpeed());
-            existingPlane.setEconomySeats(plane.getEconomySeats());
-            existingPlane.setBusinessSeats(plane.getBusinessSeats());
-            existingPlane.setTotalSeats(plane.getTotalSeats());
-            existingPlane.setCurrentLocation(plane.getCurrentLocation());
-            existingPlane.setLastLocation(plane.getLastLocation());
-            existingPlane.setStatus(plane.getStatus());
+            existingPlane.setSeatCapacity(plane.getSeatCapacity());
             planeRepository.save(existingPlane);
             return new ResponseEntity<>("Plane details updated successfully", HttpStatus.OK);
         }
